03dfa4349829f7eb3407ba65d83e2c0ff0325436
mh1sourov <sourovmh11@gmail.com>
2025-01-25 20:47:03 +0600

    some design and keyboard section's live okay

--- js/clash.js
+++ js/clash.js
@@ -1,11 +1,38 @@
-let startPlayButton = document.getElementById("startPlaybtn")
 
-startPlayButton.addEventListener("click", function(){
-    // console.log("button clicked")
-
-    // home section start here
-
-    
+let string = "abcdefghijklmnopqrstuvwxyz"
+function continueGame(){
 
 
-})
+
+let alphabets = string.split("");
+
+let randomNumber = Math.random()*25;
+let number = Math.round(randomNumber);
+// console.log(number);
+
+let alphabet  = alphabets[number];
+
+console.log(alphabet);
+
+document.getElementById("screenAlphabet").innerText = alphabet
+setBackgroundColor(alphabet)
+
+
+}
+
+function playNow(){
+    const homeSec = document.getElementById("homeSection")
+        homeSec.classList.add("hidden")
+
+        const playGround = document.getElementById("playgroundSec");
+        playGround.classList.remove("hidden")
+
+        continueGame();
+        
+}
+
+
+function setBackgroundColor(elementID){
+let specificKey = document.getElementById(elementID);
+specificKey.classList.add("bg-pink-700")
+}
