8b816d781aa23ecb231a1ce96a2c771bb10a3576
mh1sourov <sourovmh11@gmail.com>
2025-01-25 16:33:14 +0600

    all page setup complted

---
+++ js/clash.js
@@ -0,0 +1,11 @@
+let startPlayButton = document.getElementById("startPlaybtn")
+
+startPlayButton.addEventListener("click", function(){
+    // console.log("button clicked")
+
+    // home section start here
+
+    
+
+
+})
